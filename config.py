10b1d1af3fe7cf4cffde88ed3546b65c360a437a
Joe Selvik <joe@thenewtricks.com>
2014-05-03 18:35:04 -0500

    initial commit

---
+++ config.py
@@ -0,0 +1,12 @@
+SERVER_IP = 'bulbasaur.tntapp.co'
+SERVER_PORT = 6666
+
+
+REPLY_PORT = 7537
+
+
+PING_INTERVAL = 3  # seconds
+
+
+MSG_BIND = {"event": "bind", "channel": "door-1"}
+MSG_PING = {"event": "ping"}
---
+++ door_endpoint.py
@@ -0,0 +1,19 @@
+import socket
+import json
+import config
+import time
+
+
+def send_msg(sock, msg):
+    jmsg = json.dumps(msg)
+    print msg
+    sock.sendto(jmsg, (config.SERVER_IP, config.SERVER_PORT))
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+send_msg(sock, config.MSG_BIND)
+
+while True:
+    send_msg(sock, config.MSG_PING)
+    time.sleep(config.PING_INTERVAL)
