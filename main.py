6f01506b082171ad17143ebb73a4208677d65b30
Joe Selvik <joe@thenewtricks.com>
2014-05-07 18:16:01 -0500

    comment out

--- main.py
+++ main.py
@@ -6,38 +6,38 @@ import Adafruit_BBIO.PWM as PWM
 # import Adafruit_BBIO.GPIO as GPIO
 
 
-# Globals
-leds = SPI(0, 0)
+# # Globals
+# leds = SPI(0, 0)
 
 
-def init_LEDs():
-    PWM.start("P9_14", 100)		# red
-    PWM.start("P9_42", 100)		# green
-    PWM.start("P8_13", 100)		# blue
+# def init_LEDs():
+#     PWM.start("P9_14", 100)		# red
+#     PWM.start("P9_42", 100)		# green
+#     PWM.start("P8_13", 100)		# blue
 
 
-def turn_on_all_LEDs():
-    leds.writebytes([0b00000000])
+# def turn_on_all_LEDs():
+#     leds.writebytes([0b00000000])
 
 
-def turn_off_all_LEDs():
-    leds.writebytes([0b11111111])
+# def turn_off_all_LEDs():
+#     leds.writebytes([0b11111111])
 
 
-rt = RealtimeService('rt.tntapp.co', 2748)
+# rt = RealtimeService('rt.tntapp.co', 2748)
 
 
-init_LEDs()
+# init_LEDs()
 
 
 @rt.bind('door-1')
 def ken_face(data):
     if data['locked'] is True:
         print "led [ OFF    ]"
-        turn_off_all_LEDs()
+        # turn_off_all_LEDs()
     else:
         print "led [     ON ]"
-        turn_on_all_LEDs()
+        # turn_on_all_LEDs()
 
 while True:
     pass
