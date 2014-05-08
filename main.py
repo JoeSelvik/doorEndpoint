4be70376359e873257362c9cb181fbf39b22bf75
Joe Selvik <joe@thenewtricks.com>
2014-05-07 19:55:57 -0500

    some abstraction

--- main.py
+++ main.py
@@ -8,14 +8,21 @@ import Adafruit_BBIO.PWM as PWM
 from time import sleep
 
 
+# Constants
+RED = "P9_14"
+GREEN = "P9_42"
+BLUE = "P8_13"
+
+
 # Globals
 leds = SPI(0, 0)
+rt = RealtimeService('rt.tntapp.co', 2748)
 
 
 def init_LEDs():
-    PWM.start("P9_14", 100)		# red
-    PWM.start("P9_42", 100)		# green
-    PWM.start("P8_13", 100)		# blue
+    PWM.start(RED, 100)		# red
+    PWM.start(GREEN, 100)		# green
+    PWM.start(BLUE, 100)		# blue
 
 
 def turn_on_all_LEDs():
@@ -26,12 +33,8 @@ def turn_off_all_LEDs():
     leds.writebytes([0b11111111])
 
 
-rt = RealtimeService('rt.tntapp.co', 2748)
-
 init_LEDs()
-#PWM.start("P9_14", 0)
-#PWM.start("P9_42", 0)
-#PWM.start("P8_13", 0)
+
 
 @rt.bind('door-1')
 def ken_face(data):
