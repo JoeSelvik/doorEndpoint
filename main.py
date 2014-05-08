07a05cd990d117b8bce29d62900d788a9a2335ec
Joe Selvik <joe@thenewtricks.com>
2014-05-08 00:31:46 -0500

    just locks

--- main.py
+++ main.py
@@ -72,21 +72,21 @@ flash_n_times(n=5, duration=0.4)
 @rt.bind('door-1')
 def ken_face(data):
     if data['locked'] is True:
-        print "led [ OFF    ]"
-        make_lights_red()
+        print "led [ LOCK    ]"
+        # make_lights_red()
         GPIO.output(DOORPORT, GPIO.LOW)      # Lock the door
-        flash_n_times(n=3, duration=0.2)
-        turn_on_all_LEDs()
-        sleep(2)
-        flash_n_times(n=3, duration=0.2)
+        # flash_n_times(n=3, duration=0.2)
+        # turn_on_all_LEDs()
+        # sleep(2)
+        # flash_n_times(n=3, duration=0.2)
     else:
-        print "led [     ON ]"
-        make_lights_green()
-        turn_on_all_LEDs()
+        print "led [     UNLOCK ]"
+        # make_lights_green()
+        # turn_on_all_LEDs()
         GPIO.output(DOORPORT, GPIO.HIGH)    # Unlock the door
         sleep(5)
         GPIO.output(DOORPORT, GPIO.LOW)     # Lock the door
-        flash_n_times(n=3, duration=0.2)
+        # flash_n_times(n=3, duration=0.2)
 
 print "Starting doorEndpoint"
 
