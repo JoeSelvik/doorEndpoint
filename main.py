3599f51b7ad3c992b7e27f0071adf7381fdec641
Joe Selvik <joe@thenewtricks.com>
2014-05-08 00:30:50 +0000

    switched pass to sleep(1)

--- main.py
+++ main.py
@@ -5,39 +5,44 @@ import Adafruit_BBIO.PWM as PWM
 
 # import Adafruit_BBIO.GPIO as GPIO
 
-
-# # Globals
-# leds = SPI(0, 0)
+from time import sleep
 
 
-# def init_LEDs():
-#     PWM.start("P9_14", 100)		# red
-#     PWM.start("P9_42", 100)		# green
-#     PWM.start("P8_13", 100)		# blue
+# Globals
+leds = SPI(0, 0)
 
 
-# def turn_on_all_LEDs():
-#     leds.writebytes([0b00000000])
+def init_LEDs():
+    PWM.start("P9_14", 100)		# red
+    PWM.start("P9_42", 100)		# green
+    PWM.start("P8_13", 100)		# blue
 
 
-# def turn_off_all_LEDs():
-#     leds.writebytes([0b11111111])
+def turn_on_all_LEDs():
+    leds.writebytes([0b00000000])
 
 
-# rt = RealtimeService('rt.tntapp.co', 2748)
+def turn_off_all_LEDs():
+    leds.writebytes([0b11111111])
 
 
-# init_LEDs()
+rt = RealtimeService('rt.tntapp.co', 2748)
 
+init_LEDs()
+#PWM.start("P9_14", 0)
+#PWM.start("P9_42", 0)
+#PWM.start("P8_13", 0)
 
 @rt.bind('door-1')
 def ken_face(data):
     if data['locked'] is True:
         print "led [ OFF    ]"
-        # turn_off_all_LEDs()
+        turn_off_all_LEDs()
     else:
         print "led [     ON ]"
-        # turn_on_all_LEDs()
+        turn_on_all_LEDs()
+
+print "Starting doorEndpoint"
 
 while True:
-    pass
+    sleep(1)
